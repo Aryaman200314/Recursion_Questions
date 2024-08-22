226f68e24be19af71dd1cdd48024962ffc547b58
Aryaman Sharma <200314arya@gmail.com>
2024-08-23 00:53:39 +0530

    BinaryTree

--- BinaryTrees.cpp
+++ BinaryTrees.cpp
@@ -1,5 +1,33 @@
 #include<iostream>
 using namespace std;
+class Node {
+    public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int d){
+        this->data = d;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+Node* buildTree(Node* root){
+    cout<<"Enter the data at the node:"<<endl;
+    int data;
+    cin>> data;
+    if(data == -1 ){
+        return NULL;
+    }
+    cout<< "Enter the data fot the left of the node:"<<data<<endl;
+    root->left = buildTree(root->left);
+    cout<< "Enter the data fot the right of the node:"<<data<<endl;
+    root->right = buildTree(root->right);
+    return root;
+}
 int main(){
-    cout<<"hello";
+    Node* root = NULL;
+    root = buildTree(root);
+    return 0;
 }
